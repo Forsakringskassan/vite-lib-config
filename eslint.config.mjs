6bc4e1a9ef9a29ae89221ca0492915fdc0ca4298
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-05-25 21:06:34 +0200

    chore(deps): update @forsakringskassan/eslint-config monorepo to v15

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,7 @@ export default [
     defineConfig({
         files: ["*.d.ts"],
         rules: {
-            "import/extensions": "off",
+            "import-x/extensions": "off",
         },
     }),
 ];
