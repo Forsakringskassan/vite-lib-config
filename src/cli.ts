01e52861f9725e6a5aa38636fc3f62c0f8b7ce83
David Sveningsson <ext@sidvind.com>
2026-08-09 13:55:24 +0200

    feat: optionally clean dist folder

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,9 @@
+import fs from "node:fs/promises";
 import { build as viteBuild } from "vite";
 
-export async function cli(): Promise<void> {
+export async function cli(argv: string[]): Promise<void> {
+    if (argv.includes("--clean")) {
+        await fs.rm("dist", { recursive: true, force: true });
+    }
     await viteBuild();
 }
