b6eac308eaf44d59e7a76dc3af5e7466895f5cab
David Sveningsson <ext@sidvind.com>
2026-08-22 19:28:21 +0200

    refactor: use `parseArgs` from `node:util`

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,36 @@
 import fs from "node:fs/promises";
+import { parseArgs } from "node:util";
 import { build as viteBuild } from "vite";
 
-export async function cli(argv: string[]): Promise<void> {
-    if (argv.includes("--clean")) {
+export async function cli(args: string[]): Promise<void> {
+    const { values: flags } = parseArgs({
+        args,
+        strict: true,
+        allowPositionals: false,
+        options: {
+            clean: {
+                type: "boolean",
+                default: false,
+            },
+            help: {
+                type: "boolean",
+                default: false,
+            },
+        },
+    });
+
+    if (flags.help) {
+        console.log("usage: fk-build-vue-lib [OPTIONS..]");
+        console.log(`
+  --clean                    Clean dist folder before building.
+  --help                     Show this help.
+`);
+        return;
+    }
+
+    if (flags.clean) {
         await fs.rm("dist", { recursive: true, force: true });
     }
+
     await viteBuild();
 }
