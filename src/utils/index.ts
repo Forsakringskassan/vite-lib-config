5217981ca68c2245edd2034b932deefc0ef042d0
David Sveningsson <ext@sidvind.com>
2026-08-23 10:12:19 +0200

    refactor: barrel import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { detectVueMajor } from "./detect-vue-major";
 export { extractAugmentations } from "./extract-augmentations";
+export { lookupFile } from "./lookup-file";
 export { prettyList } from "./pretty-list";
 export { readJsonFile } from "./read-json-file";
